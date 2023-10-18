92b5a4a4fc57b49931aa586bea0a9bc399101b83
aninhasilvafid <147007962+aninhasilvafid@users.noreply.github.com>
2023-10-18 20:04:22 -0300

    barbearia

---
+++ style.cs
@@ -0,0 +1,41 @@
+body{
+
+}
+#baner{
+    width:100%;
+}
+
+.principal{
+    background:#CCCCCC;
+    padding:30px;
+}
+h1{
+    text-alingn:center
+}
+p{text-align:center;
+ }
+#missao{
+    font-size:20px
+    }
+em strong{
+    color:#Ff0000;
+}
+.itens{
+    font-style:italic
+    }
+.beneficios{
+    background:#FFFFFF;
+    padding:20px;
+}
+h2{
+    text-align:center;
+}
+ul{
+    display:inline-block;
+    vertical-alingn:top;
+    width:20%;
+    margin-right:15%;
+}
+.imagembeneficios{
+    width:50%;
+}
